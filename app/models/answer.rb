7f96b1266053b974a1fda25d581a3e694fc0ac5c
Neil Carvalho <me@neil.pro>
2018-10-18 21:35:38 -0300

    Set default values depending on what kind of question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,15 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  before_save :set_order
+  before_save :set_default_values
 
-  def set_order
+  def set_default_values
     self.order ||= question.answers.maximum(:order).to_i + 1
+
+    if question.single_answer?
+      self.value ||= order
+    else
+      self.value ||= 10
+    end
   end
 end
